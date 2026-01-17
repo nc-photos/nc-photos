3c7a3a80f74cbd143ce3ee88d4a094df35d8140d
nkming2 <nkming2@gmail.com>
2026-01-17 19:19:48 +0800

    Update jxlcoder

--- np_platform_uploader/android/dependency/libjxl/headers/jxl/encode.h
+++ np_platform_uploader/android/dependency/libjxl/headers/jxl/encode.h
@@ -25,7 +25,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#if defined(__cplusplus) || defined(c_plusplus)
+#ifdef __cplusplus
 extern "C" {
 #endif
 
@@ -44,7 +44,7 @@ JXL_EXPORT uint32_t JxlEncoderVersion(void);
  * Allocated and initialized with @ref JxlEncoderCreate().
  * Cleaned up and deallocated with @ref JxlEncoderDestroy().
  */
-typedef struct JxlEncoderStruct JxlEncoder;
+typedef struct JxlEncoder JxlEncoder;
 
 /**
  * Settings and metadata for a single image frame. This includes encoder options
@@ -54,7 +54,7 @@ typedef struct JxlEncoderStruct JxlEncoder;
  * Cleaned up and deallocated when the encoder is destroyed with
  * @ref JxlEncoderDestroy().
  */
-typedef struct JxlEncoderFrameSettingsStruct JxlEncoderFrameSettings;
+typedef struct JxlEncoderFrameSettings JxlEncoderFrameSettings;
 
 /**
  * Return value for multiple encoder functions.
@@ -352,7 +352,7 @@ typedef enum {
    *
    * When using streaming input and output the encoder minimizes memory usage at
    * the cost of compression density. Also note that images produced with
-   * streaming mode might not be progressively decodeable.
+   * streaming mode might not be progressively decodable.
    */
   JXL_ENC_FRAME_SETTING_BUFFERING = 34,
 
@@ -387,6 +387,11 @@ typedef enum {
    * 0 = disabled, 1 = enabled (default)
    */
   JXL_ENC_FRAME_SETTING_USE_FULL_IMAGE_HEURISTICS = 38,
+
+  /** Disable perceptual optimizations. 0 = optimizations enabled (default), 1 =
+   * optimizations disabled.
+   */
+  JXL_ENC_FRAME_SETTING_DISABLE_PERCEPTUAL_HEURISTICS = 39,
 
   /** Enum value not to be used as an option. This value is added to force the
    * C compiler to have the enum to take a known size.
@@ -999,8 +1004,9 @@ JXL_EXPORT JxlEncoderStatus JxlEncoderSetExtraChannelBuffer(
  * case metadata cannot be added.
  *
  * Each box generally has the following byte structure in the file:
- * - 4 bytes: box size including box header (Big endian. If set to 0, an
- *   8-byte 64-bit size follows instead).
+ * - 4 bytes: box size including box header (Big endian. If set to 1, an
+ *   8-byte 64-bit size follows instead. If set to 0, the box extends to the
+ *   end of the file.)
  * - 4 bytes: type, e.g. "JXL " for the signature box, "jxlc" for a codestream
  *   box.
  * - N bytes: box contents.
@@ -1501,6 +1507,8 @@ JXL_EXPORT float JxlEncoderDistanceFromQuality(float quality);
  * only @ref JxlEncoderFrameSettings created with this function for the same
  * encoder instance can be used.
  *
+ * The returned value could be NULL in case of out of memory situatiton.
+ *
  * @param enc encoder object.
  * @param source source options to copy initial values from, or NULL to get
  * defaults initialized to defaults.
@@ -1588,7 +1596,7 @@ JXL_EXPORT void JxlEncoderSetDebugImageCallback(
 JXL_EXPORT void JxlEncoderCollectStats(JxlEncoderFrameSettings* frame_settings,
                                        JxlEncoderStats* stats);
 
-#if defined(__cplusplus) || defined(c_plusplus)
+#ifdef __cplusplus
 }
 #endif
 
