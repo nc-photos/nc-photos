22e4a1bed5b181e6b88fee3e18f9bc2712783e92
nkming2 <nkming2@gmail.com>
2026-02-13 20:45:54 +0800

    Format file

--- np_platform_uploader/android/src/main/kotlin/com/nkming/nc_photos/np_platform_uploader/FormatConverter.kt
+++ np_platform_uploader/android/src/main/kotlin/com/nkming/nc_photos/np_platform_uploader/FormatConverter.kt
@@ -7,65 +7,54 @@ import com.nkming.nc_photos.np_android_core.measureTime
 import java.io.File
 
 class FormatConverter(
-	private val contentResolver: ContentResolver
+    private val contentResolver: ContentResolver
 ) {
-	companion object {
-		private const val TAG = "FormatConverter"
-	}
+    companion object {
+        private const val TAG = "FormatConverter"
+    }
 
-	enum class Format(val value: Int) {
-		JPEG(0),
-		JXL(1),
-	}
+    enum class Format(val value: Int) {
+        JPEG(0), JXL(1),
+    }
 
-	/**
-	 * @param quality Specify the quality hints when compressing to the desired
-	 * format. May not be supported by all formats.
-	 * @param downsizeMp Downsize the image to a specific MP size while
-	 * retaining the aspect ratio
-	 */
-	fun convert(
-		uri: Uri,
-		exportDir: File,
-		format: Format,
-		quality: Int,
-		downsizeMp: Double?
-	): File? {
-		val outFile = File.createTempFile("out", null, exportDir)
-		try {
-			contentResolver.openFileDescriptor(uri, "r").use {
-				if (it == null) {
-					logE(TAG, "Failed to open file uri: $uri")
-					outFile.delete()
-					return null
-				}
-				val result = measureTime(TAG, "[convert] Elapsed") {
-					convertNative(
-						it.fd, outFile.absolutePath, format.value, quality,
-						downsizeMp ?: -1.0
-					)
-				}
-				if (result != 0) {
-					logE(
-						TAG,
-						"Failed to convert file uri, result: $result, uri: $uri"
-					)
-					outFile.delete()
-					return null
-				}
-			}
-			return outFile
-		} catch (e: Throwable) {
-			outFile.delete()
-			throw e
-		}
-	}
+    /**
+     * @param quality Specify the quality hints when compressing to the desired
+     * format. May not be supported by all formats.
+     * @param downsizeMp Downsize the image to a specific MP size while
+     * retaining the aspect ratio
+     */
+    fun convert(
+        uri: Uri, exportDir: File, format: Format, quality: Int, downsizeMp: Double?
+    ): File? {
+        val outFile = File.createTempFile("out", null, exportDir)
+        try {
+            contentResolver.openFileDescriptor(uri, "r").use {
+                if (it == null) {
+                    logE(TAG, "Failed to open file uri: $uri")
+                    outFile.delete()
+                    return null
+                }
+                val result = measureTime(TAG, "[convert] Elapsed") {
+                    convertNative(
+                        it.fd, outFile.absolutePath, format.value, quality, downsizeMp ?: -1.0
+                    )
+                }
+                if (result != 0) {
+                    logE(
+                        TAG, "Failed to convert file uri, result: $result, uri: $uri"
+                    )
+                    outFile.delete()
+                    return null
+                }
+            }
+            return outFile
+        } catch (e: Throwable) {
+            outFile.delete()
+            throw e
+        }
+    }
 
-	private external fun convertNative(
-		srcFd: Int,
-		dstPath: String,
-		format: Int,
-		quality: Int,
-		downsizeMp: Double
-	): Int
+    private external fun convertNative(
+        srcFd: Int, dstPath: String, format: Int, quality: Int, downsizeMp: Double
+    ): Int
 }
