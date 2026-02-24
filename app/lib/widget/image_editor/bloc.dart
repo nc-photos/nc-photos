922f95eb4eed8faef9b8dc91b2d8d7a561eadf53
nkming2 <nkming2@gmail.com>
2026-02-25 00:35:44 +0800

    Raise preview resolution in image editor

--- app/lib/widget/image_editor/bloc.dart
+++ app/lib/widget/image_editor/bloc.dart
@@ -153,6 +153,6 @@ class _IeBloc extends Bloc<_Event, _State> with BlocLogger {
 
   static final _log = Logger("ImageEditorBloc");
 
-  static const _previewWidth = 640;
-  static const _previewHeight = 480;
+  static const _previewWidth = 1280;
+  static const _previewHeight = 1280;
 }
