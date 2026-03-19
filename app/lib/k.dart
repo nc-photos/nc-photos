aad3b4745c16bd94c84035877fc60b7e9973f6a5
nkming2 <nkming2@gmail.com>
2026-03-20 00:58:47 +0800

    85.1

--- app/lib/k.dart
+++ app/lib/k.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 /// Version string shown in settings page
-const versionStr = "85.0";
-const version = 8500;
+const versionStr = "85.1";
+const version = 8501;
 
 /// Show a snack bar for a short amount of time
 const snackBarDurationShort = Duration(seconds: 4);
